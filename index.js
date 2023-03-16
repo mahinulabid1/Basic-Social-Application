0c2fc09c382ec7145c6c4b8d21d1ce5805f7f6e6
Mahinul Mostafa Abid <mahinulmostafa@gmail.com>
2023-03-16 09:49:58 +0600

    created replace middleware

--- index.js
+++ index.js
@@ -3,19 +3,51 @@ const fs= require("fs");
 const express= require('express');
 const app = express();
 
+
+// fetching database
 const userInfo= fs.readFileSync(`${__dirname}/database/userinfo.json`, "utf-8");
-const HTMLPage= fs.readFileSync(`${__dirname}/projectSrc/index.html`);
-const page1= fs.readFileSync(`${__dirname}/projectSrc/page1.html`);
-const page2= fs.readFileSync(`${__dirname}/projectSrc/page2.html`);
 
 
-app.get("/", (req,res)=>{
-    let y = JSON.parse(userInfo)
-    console.log(y.mahinul9);
+//fetching component
+const pageHeader= fs.readFileSync(`${__dirname}/projectSrc/component/header.html`, "utf-8");
+
+
+// fetching stylesheet
+const indexPageStyle= fs.readFileSync(`${__dirname}/projectSrc/src/css/index.css`, "utf-8");
+
+
+// fetching HTML page
+const HTMLPage= fs.readFileSync(`${__dirname}/projectSrc/index.html`, "utf-8");
+const page1= fs.readFileSync(`${__dirname}/projectSrc/page1.html`, "utf-8");
+const page2= fs.readFileSync(`${__dirname}/projectSrc/page2.html`, "utf-8");
+
+
+// replace middleware
+let replacedCode;
+const replaceCode=(element)=>{
+    let replaceTemp;
+    if(element === HTMLPage){
+        replaceTemp = pageHeader.replace("%%HeaderTitle%%", "Connectify-Home");
+        replaceTemp= replaceTemp.replace("%%styles%%", indexPageStyle);
+    }
+    else{
+        console.log("no page title"); 
+    }
+
+    replacedCode= element.replace("%%Head%%", replaceTemp);
+}
+
+
+
+app.get("/", (req,res)=>{ 
+    // let y = JSON.parse(userInfo)
+    // console.log(y.mahinul9);
+    replaceCode(HTMLPage);
     res.writeHead(200, {
-        "content-type":"text/html"
-    })
-    res.end(HTMLPage);
+        "content-type":"text/html" 
+    });
+
+    res.end(replacedCode);
 });
 
 
