69a0e6be199717252d3b03f366fe62153197b0fd
Mahinul Mostafa Abid <mahinulmostafa@gmail.com>
2023-03-16 15:48:01 +0600

    added authentication function

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const fs= require('fs');
+
 exports.moduleFunction={
     BindComponent:(bindarr)=>{
         // BIND COMPONENT TAKES TWO ARGUMENT
@@ -10,5 +12,39 @@ exports.moduleFunction={
 
         return tempmemory;
         // this returns a binded chunk of same group code like js, css, html
+    },
+    Authentication:(username,password)=>{
+        let Switch;
+        let passwordFromData;
+        let userNameCheck= fs.readFileSync(`${__dirname}/database/userinfo.json`);
+        userNameCheck= JSON.parse(userNameCheck);
+
+        for(let i=0;i<userNameCheck.length;i++){
+            console.log(userNameCheck[i]);
+
+            if(userNameCheck[i].userid===username){
+                //this means username matched
+                passwordFromData=userNameCheck[i].password;
+                console.log("username matched");
+                //if username found in database then return true
+                Switch=true; 
+                break;
+            }else{
+                Switch=false;
+                passwordFromData="null";
+            }
+        }
+
+        if(Switch===true){
+            //check if password match
+            if(password ===passwordFromData){
+                console.log("password match");
+                return true;
+            }else{
+                console.log("password didnt match")
+            }
+        }
+
+        
     }
 }
