e45f4431798915d861d0021df5f5a2feb6cfda5b
Mahinul Mostafa Abid <mahinulmostafa@gmail.com>
2023-03-16 15:48:22 +0600

    added authentication function

--- index.js
+++ index.js
@@ -13,7 +13,14 @@
 const fs= require("fs");
 const express= require('express');
 const app = express();
+const cookieParser=require("cookie-parser");
+app.use(express.json());
+app.use(cookieParser());
 
+
+
+// app.use(express.urlencoded()); //this one is deprecated
+app.use(express.urlencoded({ extended: true }));
 
 
 const functionsModule = require(`${__dirname}/functions.js`);
@@ -61,14 +68,15 @@ const replaceCode=(element)=>{
     let replaceTemp;
     if(element === FileSystem.HTMLPage){
         replaceTemp = pageHeader.replace("%%HeaderTitle%%", "Connectify-Home");
-        
+        replaceTemp= replaceTemp.replace("<!-- **icon** -->", `<link rel="icon/image" href="${__dirname}/projectSrc/src/img/favicon.png">`)
         //BIND styles
         let BINDED = ModuleBind.BindComponent([FileSystem.HomePageStyles,FileSystem.UniversalStyles, FileSystem.Navigation]);
         replaceTemp= replaceTemp.replace("/*%%styles%%*/", BINDED);
     }
     else if(element===FileSystem.LoginPage){
-        replaceTemp = pageHeader.replace("%%HeaderTitle%%", "Connectify-Login");
-        replaceTemp= replaceTemp.replace("/*%%styles%%*/", FileSystem.LoginPageStyle);
+        replaceTemp = pageHeader.replace("%%HeaderTitle%%", "Connectify-Login"); 
+        let BINDED = ModuleBind.BindComponent([FileSystem.LoginPageStyles,FileSystem.UniversalStyles, FileSystem.Navigation]);
+        replaceTemp= replaceTemp.replace("/*%%styles%%*/", BINDED);
     }
     else{
         console.log("no page title"); 
@@ -84,7 +92,6 @@ app.get("/", (req,res)=>{
     res.writeHead(200, {
         "content-type":"text/html" 
     });
-    // console.log(fileManagement.hello.first);
 
     res.end(replacedCode);
 });
@@ -118,6 +125,25 @@ app.get("/Page2", (req,res)=>{
     })
     res.end(FileSystem.page2);
 });
+
+
+//login section
+app.post("/login",(req,res)=>{
+    let username= req.body.username;
+    let password=req.body.password;
+    
+    //now I have the password, what should I do?
+    //it will route to specific 
+    let authResult =ModuleBind.Authentication(username, password);
+    if(authResult===true){
+        console.log("continue to login");
+        res.cookie("username",username);
+        res.cookie('password', password);
+        res.redirect("/");
+    }
+    // res.end(replaceCode(FileSystem.HTMLPage));
+     
+})
 
 
 
